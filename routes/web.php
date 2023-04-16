05b07d1e7581e2af71e3ba4a90b87db6e1f85e12
untaravc <vyvy1777@gmail.com>
2023-04-16 16:10:52 +0700

    notify active

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AuthController;
 
+Route::get('/', function (){
+    return 'perki-src';
+});
 Route::get('/panel', [AuthController::class, 'adminPanel']);
 Route::get('/panel/{path}', [AuthController::class, 'adminPanel'])->where( 'path' , '([A-z\d\-\/_.]+)?' );
 
