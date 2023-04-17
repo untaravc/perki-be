1e1cee6e3683eb41985d16678c18f8f94d344917
untaravc <vyvy1777@gmail.com>
2023-04-17 12:41:43 +0700

    server config

--- app/Http/Controllers/System/UserInitController.php
+++ app/Http/Controllers/System/UserInitController.php
@@ -28,7 +28,6 @@ class UserInitController extends Controller
                 "type"                  => 'admin',
                 "otp"                   => null,
                 "status"                => 100,
-                "remember_token"        => null,
             ],
             [
                 "is_speaker"            => 0,
@@ -47,7 +46,6 @@ class UserInitController extends Controller
                 "type"                  => 'user',
                 "otp"                   => null,
                 "status"                => 100,
-                "remember_token"        => null,
             ],
         ];
 
